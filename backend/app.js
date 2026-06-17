fa6f548257b6de67b772b912126ab58b308ec7c1
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-06-17 10:58:04 +0200

    mongoose + models Book & User

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const dns = require('node:dns');
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
+const app = express();
+
+mongoose.connect('mongodb+srv://Daeliora2:GoTest@cluster0.gmy0xny.mongodb.net',
+)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((err) => console.log('Connexion à MongoDB échouée !', err));
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
+
---
+++ backend/models/Book.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const ratingSchema = mongoose.Schema({
+  userId: String,
+  grade: Number
+});
+
+const bookSchema = mongoose.Schema({
+  userId: String,
+  title: String,
+  author: String,
+  imageUrl: String,
+  year: Number,
+  genre: String,
+  ratings: [ratingSchema],
+  averageRating: Number
+});
+
+module.exports = mongoose.model('Book', bookSchema);
