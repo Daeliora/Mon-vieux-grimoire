c4e33c844f28ff4c5ae0f74a8f0289a3abb9bd36
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-06-17 16:55:55 +0200

    rating + best rated

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -92,11 +92,41 @@ exports.deleteBook = (req, res, next) => {
 };
 
 exports.getBestRatedBooks = (req, res, next) => {
-  res.status(200).json([]);
+  Book.find()
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .then(books => {
+      res.status(200).json(books);
+    })
+    .catch(error => {
+      res.status(400).json({ error });
+    });
 };
 
 exports.rateBook = (req, res, next) => {
-  res.status(200).json({
-    message: 'Note ajoutée'
-  });
+  Book.findOne({ _id: req.params.id })
+    .then(book => {
+      const alreadyRated = book.ratings.find(rating => rating.userId === req.auth.userId);
+
+      if (alreadyRated) {
+        return res.status(400).json({message: 'Livre déjà noté'});
+      }
+
+      book.ratings.push({
+        userId: req.auth.userId,
+        grade: req.body.rating
+      });
+
+      const total = book.ratings.reduce(
+        (sum, rating) => sum + rating.grade,
+        0
+      );
+
+      book.averageRating = total / book.ratings.length;
+
+      book.save()
+        .then(updatedBook => {res.status(200).json(updatedBook)})
+        .catch(error => {res.status(400).json({ error })});
+    })
+    .catch(error => {res.status(404).json({ error })});
 };
