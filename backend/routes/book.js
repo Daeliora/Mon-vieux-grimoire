5b512a4604c4bc12882abc97c5c8e21f8b0dc09f
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-06-17 15:32:08 +0200

    multer

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const booksCtrl = require('../controllers/books');
 
@@ -8,9 +9,9 @@ router.get('/', auth, booksCtrl.getAllBooks);
 
 router.get('/:id', auth, booksCtrl.getOneBook);
 
-router.post('/', auth, booksCtrl.createBook);
+router.post('/', auth, multer, booksCtrl.createBook);
 
-router.put('/:id', auth, booksCtrl.modifyBook);
+router.put('/:id', auth, multer, booksCtrl.modifyBook);
 
 router.delete('/:id', auth, booksCtrl.deleteBook);
 
