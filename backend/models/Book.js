d105f41437bc93f56d93cdcf81e6be6ea05da4d9
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-06-17 15:26:36 +0200

    correction Book + correction pb  PORT backend

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
 const ratingSchema = mongoose.Schema({
-  userId: String,
-  grade: Number
+  userId: { type: String, required: true },
+  grade: { type: Number, required: true }
 });
 
 const bookSchema = mongoose.Schema({
-  userId: String,
-  title: String,
-  author: String,
-  imageUrl: String,
-  year: Number,
-  genre: String,
+  userId: { type: String, required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
   ratings: [ratingSchema],
-  averageRating: Number
+  averageRating: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Book', bookSchema);
