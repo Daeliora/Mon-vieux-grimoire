ea5be33c1fcfa5e6e26700aa5c96365efc635398
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-06-17 11:54:14 +0200

    authentification

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
@@ -23,7 +24,46 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-  res.status(200).json({
-    message: 'Login OK'
-  });
+  User.findOne({
+    email: req.body.email
+  })
+    .then(user => {
+
+      if (!user) {
+        return res.status(401).json({
+          error: 'Utilisateur non trouvé !'
+        });
+      }
+
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+
+          if (!valid) {
+            return res.status(401).json({
+              error: 'Mot de passe incorrect !'
+            });
+          }
+
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              {
+                userId: user._id
+              },
+              'RANDOM_TOKEN_SECRET',
+              {
+                expiresIn: '24h'
+              }
+            )
+          });
+
+        })
+        .catch(error =>
+          res.status(500).json({ error })
+        );
+
+    })
+    .catch(error =>
+      res.status(500).json({ error })
+    );
 };
